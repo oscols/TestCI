ffce8671683193e8ea6ea05ebf4f544af1efb731
Oscar Olsson <oscols@kth.se>
2023-02-07 08:57:47 +0100

    Added some code with tests

---
+++ app/src/main/java/TestCI/Calculator.java
@@ -0,0 +1,22 @@
+package TestCI;
+
+public class Calculator {
+
+    private int value;
+
+    public Calculator() {
+        this.value = 0;
+    }
+
+    public void add(int number) {
+        this.value = this.value + number;
+    }
+
+    public void subtract(int number) {
+        this.value = this.value - number;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
